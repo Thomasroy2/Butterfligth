2e17c527909fa0bcecbf2ad456d52f1950c567f1
Nico42 <nicolas.gamin@ynov.com>
2017-11-02 16:53:17 +0100

    generate butterfly controller, manque le check en base

---
+++ Api/controllers/checkDataBase.controller.ts
@@ -0,0 +1,15 @@
+class checkDataBase {
+    
+        id      : number
+        table   : string
+       
+        constructor() {
+            this.id     = null
+            this.table  = null
+        }
+    
+        getparam2() {
+    
+            return null
+        }
+    }
---
+++ Api/controllers/generateButterfly.controller.ts
@@ -0,0 +1,83 @@
+class generateButterfly {
+    
+        idButterfly : number 
+        skill1      : number
+        skill2      : number
+        skill3      : number
+        idmin       : number 
+        
+       
+        constructor() {
+            this.idButterfly = null;
+            this.skill1 = null;
+            this.skill2 = null;
+            this.skill3 = null;
+            this.idmin = 1;      
+        }
+        
+        // renvois un id aléatoire existant en base 
+        private SelectButterFly(idmax : number, ) {
+            //rendom nombre
+            this.idButterfly = Math.floor(Math.random()*(idmax-this.idmin)+this.idmin)
+            //si id existe pas retry random id
+            while (this.checkIdDB(this.idButterfly, "Butterfly") == false){
+            this.idButterfly = Math.floor(Math.random()*(idmax-this.idmin)+this.idmin)
+            }
+            return this.idButterfly                          
+        }
+
+        // renvois un id aléatoire existant en base
+        private SelectSkill(idmax :number) : number {
+            let skill : number
+            skill =  Math.floor(Math.random()*(idmax-this.idmin)+this.idmin)
+            while (this.checkIdDB(skill, "Attack") == false){
+                skill = Math.floor(Math.random()*(idmax-this.idmin)+this.idmin)
+                }
+                return skill 
+        }
+
+       // affect les trois skills
+        private Select3Skills(idmax :number){
+
+            this.skill1 = this.SelectSkill(idmax)
+            this.skill2 = this.SelectSkill(idmax)
+            this.skill3 = this.SelectSkill(idmax)
+           
+
+            return null
+        }
+
+        //vérifie que les 3 skills sont différents
+        private CheckSkills(idmax : number){
+            while(this.skill1 == this.skill2){
+                this.skill2 = Math.floor(Math.random()*(idmax-this.idmin)+this.idmin)
+                }
+            while((this.skill1 == this.skill3)&&(this.skill2 == this.skill3)){
+                    this.skill3 = Math.floor(Math.random()*(idmax-this.idmin)+this.idmin)
+                }
+        }
+
+
+        public getButterflyJson(){
+            let dataButterfly: number[] = [this.idButterfly,this.skill1,this.skill2,this.skill3];
+            return JSON.stringify(dataButterfly)
+
+        }
+
+        // check le nombre d'id max d'une table
+        private checkIdMaxDB (id : number, table : string): boolean{
+
+            //temp
+            return true
+
+        }
+
+
+        //verifie que la donnée existe
+        private checkIdDB(id : number, table : string): boolean{
+
+            //temp
+            return true
+
+        }
+    }
