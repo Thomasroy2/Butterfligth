cbaebf91f75fdd0b359d3553b2b60dceffb14c12
Maxime Soubiran <maxime.soubiran@gmail.com>
2017-11-24 15:02:46 +0100

    implementation of socket.io

--- newAPI/app.js
+++ newAPI/app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 // Set up the express app
 const app = express();
 
@@ -17,5 +19,60 @@ require('./server/routes')(app);
 app.get('*', (req, res) => res.status(200).send({
   message: 'Welcome to the beginning of nothingness.',
 }));
+/**
+ * connexion au lancement de l'application
+ */
+io.on('connection', function(socket)
+{
+  /**
+   * Reception d'attaque et envoie de la notification au client.
+   */
+  socket.on('attack', function(type){
+    io.emit('attack','attaque reçue');
+    /**
+     * Changer la valeur de la fonction pour l'attaque .
+    */
+    //io.emit(type.room,attackAction(type));
+    //io.emit(type.room+'bet',attackAction(type));
+  });
+  /**
+   * Rejoindre la salle
+   */
+  socket.on('room',function(room){
+    if(room.combat==true)
+    {
+      socket.join(room);
+    }
+    else
+    {
+      socket.join(room+'bet');
+    }
+  });
 
+  /**
+ * Envoi de message sur le chat de paris.
+ */
+
+  socket.on('betchat',function(message){
+    
+    io.emit(message.room,message.msg);
+  });
+/**
+ * Reception de paris.
+ */
+  socket.on('bet',function(bet)
+  {
+    io.emit('bet','Pari reçu');
+    /**
+     * Changer la valeur de la fonction pour le pari .
+    */
+    //io.emit('bet',betAction(bet));
+  })
+});
+/**
+ * Lancement de l'écoute de notre serveur
+ */
+http.listen(port,function(){
+  console.log('Le serveur socket io est prêt');
+});
 module.exports = app;
